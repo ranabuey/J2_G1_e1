df05b6b865f0f9e4499c5c7008b17b30adaf823b
SofiCatalano16 <141649394+SofiCatalano16@users.noreply.github.com>
2023-08-08 12:07:32 -0300

    soy sofia

--- src/j2_g1_e1/J2_G1_e1.java
+++ src/j2_g1_e1/J2_G1_e1.java
@@ -20,6 +20,7 @@ public class J2_G1_e1 {
         System.out.println("Soy titus");
         System.out.println("Soy Bea");
         System.out.println("polank@gmail.com");
+        System.out.println("Soy Sofía");
     }
     
 }
