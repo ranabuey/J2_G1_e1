a592d6a945d3b00ec206e78322b119fd354d613a
Ben <rbenorlando@gmail.com>
2023-08-08 10:49:18 -0300

    Soy ruben comitiando

--- src/j2_g1_e1/J2_G1_e1.java
+++ src/j2_g1_e1/J2_G1_e1.java
@@ -15,7 +15,7 @@ public class J2_G1_e1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Soy Ruben");
     }
     
 }
